b767a358223ce0bda96eb4443370d0b89bc2e152
404goblintime <79607814+404goblintime@users.noreply.github.com>
2022-03-03 18:08:12 -0800

    Update main.js

--- main.js
+++ main.js
@@ -18,3 +18,48 @@ function snapshot() {
 }
 console.log('ml5 version', ml5.version);
 classifier = ml5.imageclassifier('https://teachablemachine.withgoogle.com/model.json',modelloaded)
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+ 
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+ 
+function gotResult(error, results) {
+    if (error) {
+ console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("hand_result1").innerHTML = results[0].label;
+        document.getElementById("hand_result2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if (results[0].label == "thumbs up") {
+            document.getElementById("update_emoji1").innerHTML = "&#128077";
+        }
+ 
+        if (results[0].label == "thumbs down") {
+            document.getElementById("update_emoji1").innerHTML = "&#128078";
+        }
+        if (results[0].label == "peace sign") {
+            document.getElementById("update_emoji1").innerHTML = "&#9996";
+        }
+        if (results[1].label == "thumbs up") {
+            document.getElementById("update_emoji2").innerHTML = "&#128077";
+        }
+ 
+        if (results[1].label == "thumbs down") {
+            document.getElementById("update_emoji2").innerHTML = "&#128078";
+        }
+        if (results[1].label == "peace sign") {
+            document.getElementById("update_emoji2").innerHTML = "&#9996";
+        }
+    }
+}
